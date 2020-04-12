22cf862c2822494a70cb5fd1f89343ff14d4cafb
Juliana Marin <juliana.marin04@gmail.com>
2020-04-11 19:18:52 -0500

    Components angular

--- vuelosUI/src/app/app.module.ts
+++ vuelosUI/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteComponent } from './componente/componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteComponent
   ],
   imports: [
     BrowserModule,
---
+++ vuelosUI/src/app/componente/componente.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente',
+  templateUrl: './componente.component.html',
+  styleUrls: ['./componente.component.css']
+})
+export class ComponenteComponent implements OnInit {
+
+  title = 'Bienvenido a angular';
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+
+}
