dc28df36d0c70632dd87ebb61d86c6cd43de390f
Juliana Marin <juliana.marin04@gmail.com>
2020-04-20 22:21:47 -0500

    Internacionalizacion en dos idiomas: ingles y chino

--- vuelosUI/src/app/app.component.ts
+++ vuelosUI/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +10,11 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   
-  constructor(private router:Router){}
+  constructor(private router:Router, public translate: TranslateService){
+    this.translate.addLangs(['es','en','zh']);
+    this.translate.setDefaultLang('es');
+    this.translate.use('zh');
+  }
 
   Listar(){
     this.router.navigate(["listar"]);
--- vuelosUI/src/app/app.module.ts
+++ vuelosUI/src/app/app.module.ts
@@ -8,10 +8,15 @@ import { ActualizarComponent } from './Cliente/actualizar/actualizar.component';
 import { AgregarComponent } from './Cliente/agregar/agregar.component';
 import {FormsModule} from '@angular/forms';
 import {ServiceService} from '../app/Service/service.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { ListarVuelosComponent } from './Vuelo/listar-vuelos/listar-vuelos.component';
 import { FiltroPipe } from './filtro.pipe';
-
+import {TranslateHttpLoader} from '@ngx-translate/http-loader'
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+  
+export function HttpLoaderFactory (http: HttpClient){
+  return new TranslateHttpLoader(http,'./assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -27,7 +32,15 @@ import { FiltroPipe } from './filtro.pipe';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule, 
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+    
 
   ],
   providers: [ServiceService],
